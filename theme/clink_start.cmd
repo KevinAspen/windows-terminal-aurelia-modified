5e0f7c484f1020115dfdc58ecb9bef413c6a34c7
KevinAspen <kevin.aspen7@gmail.com>
2023-01-30 16:38:02 +0000

    Update clink_start.cmd

--- theme/clink_start.cmd
+++ theme/clink_start.cmd
@@ -1,4 +1,4 @@
 @echo off
 cls;
 :: Modify to match location of start_message_clink.txt
-type "C:\Users\Kevin\Documents\WindowsPowerShell\start_message_clink.txt"
+type "C:\Users\PATH\TO\start_message_clink.txt"
