b7df2955c83c73b686805a30b3c5a011c7dceefb
KevinAspen <kevin.aspen7@gmail.com>
2023-01-30 16:38:20 +0000

    Update oh-my-posh.lua

--- theme/oh-my-posh.lua
+++ theme/oh-my-posh.lua
@@ -1,1 +1,1 @@
-load(io.popen('oh-my-posh init cmd --config "C:/Users/Kevin/Documents/WindowsPowerShell/purple-man.omp.json"'):read("*a"))()
+load(io.popen('oh-my-posh init cmd --config "C:/Users/PATH/TO/purple-man.omp.json"'):read("*a"))()
